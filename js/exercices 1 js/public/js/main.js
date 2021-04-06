f00e769cbc4e77f9a88c8fadb1db3bbca03184ca
Macluffy <ahriga.a@hotmail.com>
2021-04-06 20:05:28 +0200

    CodingWeek

---
+++ js/exercices 1 js/public/js/main.js
@@ -0,0 +1,22 @@
+
+// let prenom = (' ali ');
+// let age = (' 26 ');
+// let adresse = (" et j'habite schaerbeek ")
+// console.log ("je m'appel "+ prenom + "et j'ai" + age + " ans " + adresse) 
+// age = 26;
+// console.log(age*10)
+
+// let a=5
+// let b=8
+// let c=10
+// let d=a
+
+// console.log (a=b)
+// console.log (b=c)
+// console.log (c=d)
+
+let nom= prompt (" quel est ton nom ")
+let prenom= prompt (" quel est ton prenom ")
+let age= prompt (" quel est ton age ")
+console.log(" bonjour je m'appel " + nom +" "+ prenom  + " et j'ai "+ age)
+
